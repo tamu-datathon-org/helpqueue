787fa84e374fa674a8fab52d3c3d96cf18f25ec0
Christopher Vu <christophervu03@gmail.com>
2023-10-19 20:17:21 -0500

    correct session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
         {/* change basepath to http://localhost:3000/help/api/auth if in dev */}
-      {/* <SessionProvider session={pageProps.session} basePath='https://tamudatathon.com/help/api/auth'> */}
-      <SessionProvider session={pageProps.session} basePath='http://localhost:3000/help/api/auth'>
+      <SessionProvider session={pageProps.session} basePath='https://tamudatathon.com/help/api/auth'>
+      {/* <SessionProvider session={pageProps.session} basePath='http://localhost:3000/help/api/auth'> */}
         <Component {...pageProps} />
       </SessionProvider>
     </ChakraProvider>
