15a5680d03dd32f7efbeccba1258917ec37d8e91
Christopher Vu <christophervu03@gmail.com>
2023-10-28 10:28:26 -0500

    adding bear

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -4,14 +4,28 @@ import CSS from "csstype";
 import {signIn} from "next-auth/react";
 
 const backgroundProps: CSS.Properties = {
-  backgroundImage: "url(../images/th-cloudy-sky.png)",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover"
+};
+
+const questionBearProps: CSS.Properties = {
+    backgroundImage: "url(../help/images/landing_img.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    width: "500px",
+    height: "250px",
+};
+
+const center: CSS.Properties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
 };
 
 export default function Landing() {
   return (<div className="md:items-center h-screen flex justify-center" style={backgroundProps}>
     <div className="welcomem mt-48 md:mt-0">
+    <div style={center}><div style={questionBearProps}></div></div>
       <p className="text-center relative bottom-[20px] font-bold text-4xl sm:text-[65px] pb-6">
         Welcome to Datathon&apos;s Help Queue!
       </p>
