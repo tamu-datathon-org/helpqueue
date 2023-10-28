b032ff06221fa0e0cc38271307c28bff1a09f188
Christopher Vu <christophervu03@gmail.com>
2023-10-28 10:45:39 -0500

    added new challenge options

--- components/home/Submit.tsx
+++ components/home/Submit.tsx
@@ -20,7 +20,7 @@ export default function Submit(props: { user: User; ticket: Ticket }) {
   const { mutate } = useSWRConfig();
   const [submitLoading, setSubmitLoading] = useState(false);
 
-  const challengeOptions = ['TD Hospital Exploration', 'TD Pictionary Purge', 'TD Bots Race', 'TD Build Your Own', 'Miscellaneous'];
+  const challengeOptions = ['TD Hospital Exploration', 'TD Pictionary Purge', 'TD Bots Race', 'TD Build Your Own', 'Marky Challenge', 'MLH Challenge', 'Miscellaneous'];
 
   if (isLoading || error) {
     return <Loading />;
